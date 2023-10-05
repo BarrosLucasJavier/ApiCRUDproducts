a27be3d4d31b9d233139754679840d3cd9767eb1
Barros Lucas <lucasbarros78@hotmail.com>
2023-10-05 14:29:39 -0300

    Agregado pagina de inicio

--- src/app.js
+++ src/app.js
@@ -1,13 +1,21 @@
 import express  from "express";
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // Import products routes
 import productRouter from './routes/products.routes.js'
+import homeRouter from "./routes/home.routes.js";
+
 
 const app = express();
 app.use(express.json());
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
+app.use("/", homeRouter)
 app.use("/api/v1/products", productRouter)
 
 export default app
---
+++ src/controllers/home.controller.js
@@ -0,0 +1,6 @@
+const homeController ={
+    home:(req,res)=>{
+        res.render('home')
+    }
+}
+export default homeController;
