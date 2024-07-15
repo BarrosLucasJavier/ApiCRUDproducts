3f19509a48b2b7e73c1abe6408fa4ba352045137
Barros Lucas Javier <lucasbcco@gmail.com>
2024-07-15 09:17:13 -0300

    reestructure proyect

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
+import express  from "express";
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import  methodOverride  from 'method-override'
 import { connectToDB } from './utils/mongoose.js'
-import app from './app.js'
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+// Import products routes
+import productRouter from './routes/products.routes.js'
+import homeRouter from "./routes/home.routes.js";
+
+
+const app = express();
+app.use(express.json());
+app.use(express.static('public'));
+app.use(methodOverride('_method'));
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+
+app.use("/", homeRouter)
+app.use("/api/v1/products", productRouter)
 
 async function main(){
 
